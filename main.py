582cf5a36677cdaa9caa1b690b16bc86cbd277f7
Idit <cohen.idit10@gmail.com>
2022-03-13 15:04:03 +0200

    init file

---
+++ main.py
@@ -0,0 +1,32 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def read_file(file_name):
+    df = pd.read_csv(file_name)
+    return df
+
+
+def xy_plot(df):
+    plt.hist(df['serum_sodium'], bins=100)
+    plt.ylabel('Count')
+    plt.xlabel('Level of serum sodium in the blood (mEq/L)')
+    plt.title("Sodium Distribution")
+    #show()
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    df = read_file('data/heart_failure_clinical_records_dataset.csv')
+    women_df = df[df['sex'] == 0]
+    men_df = df[df['sex'] == 0]
+    xy_plot(women_df)
+    xy_plot(men_df)
+    print(df.columns)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
