2c833bd2f8cafd00f23e8e0af4520a7ffc0e9605
Idit <cohen.idit10@gmail.com>
2022-03-21 14:09:48 +0200

    add roc

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, roc_curve, auc, roc_auc_score, ConfusionMatrixDisplay
 from sklearn.metrics import precision_score, f1_score, recall_score
 
 import numpy as np
@@ -39,6 +39,24 @@ def metrics_scoring(y_test, y_pred):
     recall = recall_score(y_test, y_pred, average='weighted')
     return precision
 
+
+def cm_display(y_test, y_pred):
+    cm = confusion_matrix(y_test, y_pred)
+    cm_display = ConfusionMatrixDisplay(cm).plot()
+
+
+def roc(X_test, y_test, clf):
+    # define metrics
+    y_pred_proba = clf.predict_proba(X_test)[::, 1]
+    fpr, tpr, _ = roc_curve(y_test, y_pred_proba)
+
+    # create ROC curve
+    plt.plot(fpr, tpr)
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.show()
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df = read_file('data/heart_failure_clinical_records_dataset.csv')
@@ -49,11 +67,11 @@ if __name__ == '__main__':
     X = df.drop(['DEATH_EVENT'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
     clf = train_randomforestclassifier(X_train, X_test, y_train, y_test)
-    y_score = clf.fit(X_train, y_train).decision_function(X_test)
     y_pred = clf.predict(X_test)
+    roc(X_test, y_test, clf)
     metrics_scoring(y_test, y_pred)
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    precision = precision_score(y_test, y_pred, average='weighted')
+    cm_display(y_test, y_pred)
+
 
     xy_plot(women_df)
     xy_plot(men_df)
