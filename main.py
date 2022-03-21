5e43d012a72ebff7d94b704c09f334a20083d2bc
Idit <cohen.idit10@gmail.com>
2022-03-21 12:29:41 +0200

    add metrics_scoring

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import precision_score
+from sklearn.metrics import precision_score, f1_score, recall_score
+
 import numpy as np
 
 
@@ -25,8 +26,18 @@ def xy_plot(df):
     #show()
 
 
+def train_randomforestclassifier(X_train, X_test, y_train, y_test):
+    clf = RandomForestClassifier(max_depth=2, random_state=0)
+    clf.fit(X_train, y_train)
+    return clf
 
 
+def metrics_scoring(y_test, y_pred):
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    precision = precision_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    return precision
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -37,11 +48,13 @@ if __name__ == '__main__':
     y = df[['DEATH_EVENT']].copy()
     X = df.drop(['DEATH_EVENT'], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-    clf = RandomForestClassifier(max_depth=2, random_state=0)
-    clf.fit(X_train, y_train)
+    clf = train_randomforestclassifier(X_train, X_test, y_train, y_test)
+    y_score = clf.fit(X_train, y_train).decision_function(X_test)
     y_pred = clf.predict(X_test)
+    metrics_scoring(y_test, y_pred)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
     precision = precision_score(y_test, y_pred, average='weighted')
+
     xy_plot(women_df)
     xy_plot(men_df)
 
